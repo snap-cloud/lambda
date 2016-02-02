2e726b4093e36ef1a28fecd86e4f378fb287e347
Michael Ball <cycomachead@gmail.com>
2016-02-02 03:42:05 -0800

    Move more code out of modules...

--- app/controllers/dce_lti/sessions_controller.rb
+++ app/controllers/dce_lti/sessions_controller.rb
@@ -1,20 +1,18 @@
 require 'oauth/request_proxy/rack_request'
 
-module DceLti
-  class SessionsController < ApplicationController
-    include SessionHelpers
+class SessionsController < ApplicationController
+  include SessionHelpers
 
-    skip_before_filter :verify_authenticity_token, :authenticate_via_lti
+  skip_before_filter :verify_authenticity_token, :authenticate_via_lti
 
-    def create
-      if valid_lti_request?(request)
-        user = UserInitializer.find_from(tool_provider)
-        session[:current_user_id] = user.id
-        session.merge!(captured_attributes_from(tool_provider))
-        redirect_to redirect_after_successful_auth
-      else
-        render :invalid
-      end
+  def create
+    if valid_lti_request?(request)
+      user = UserInitializer.find_from(tool_provider)
+      session[:current_user_id] = user.id
+      session.merge!(captured_attributes_from(tool_provider))
+      redirect_to redirect_after_successful_auth
+    else
+      render :invalid
     end
   end
 end
