969552e7d3aac3cd97d952ac6360185cb64ef120
Michael Ball <cycomachead@gmail.com>
2016-02-25 18:02:38 -0800

    update snap for tables!

--- public/snap-path.js
+++ public/snap-path.js
@@ -1,3 +1,4 @@
-IDE_Morph.prototype.resourceURL = function (path) {
-    return '/snap/snap/' + path;
+IDE_Morph.prototype.resourceURL = function () {
+    var args = Array.prototype.slice.call(arguments, 0);
+    return '/snap/snap/' + args.join('/');
 };
