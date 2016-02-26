b53ca9be446470f34e377d8ad239f960b1f72252
Michael Ball <cycomachead@gmail.com>
2016-02-26 02:11:09 -0800

    WIP cleanup submodules part 1

--- public/snap-path.js
+++ public/snap-path.js
@@ -1,6 +1,6 @@
 IDE_Morph.prototype.resourceURL = function () {
     var args = Array.prototype.slice.call(arguments, 0);
-    return '/snap/snap/' + args.join('/');
+    return '/snap/' + args.join('/');
 };
 
 // TODO: Move this:
