0a162a04abd0c9aa7d7ae42789648aaca6e90b4a
Michael Ball <cycomachead@gmail.com>
2016-03-05 17:54:30 -0600

    Cleanup the logger

--- config/initializers/ar_log_cleaner.rb
+++ config/initializers/ar_log_cleaner.rb
@@ -8,21 +8,11 @@ class ActiveRecord::ConnectionAdapters::AbstractAdapter
 
    def log_with_binary_truncate(sql, name="SQL", binds=[], stmt_key='', &block)
      binds = binds.map do |col, data|
-       puts 'NAME'
-       puts col.name
-       if col.type == :binary && data.is_a?(String) && data.size > 27
-         data = "#{data[0,10]}[REDACTED #{data.size - 20} bytes]#{data[-10,10]}"
-       end
        if col.name == 'code_submission' || col.name == 'test_results'
          data = "#{data[0,25]} [REDACTED #{data.size - 50} bytes]  #{data[-25,25]}"
        end
        [col, data]
      end
-
-     sql = sql.gsub(/(?<='\\x[0-9a-f]{20})[0-9a-f]{20,}?(?=[0-9a-f]{20}')/) do |match|
-       "[REDACTED #{match.size} chars]"
-     end
-
      log_without_binary_truncate(sql, name, binds, stmt_key, &block)
    end
 
