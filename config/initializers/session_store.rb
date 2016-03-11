90b1b61b0c9f9bc74cb38714f65bc4ee7559062a
Michael Ball <cycomachead@gmail.com>
2016-03-11 02:09:59 -0800

    Bump expiration on sessions to 2 hours, like a lab period

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_lambda_session'
+
 # LTI Keys
-Rails.application.config.session_store :active_record_store, key: '_lambda_ar_store', expire_after: 60.minutes
+Rails.application.config.session_store :active_record_store, key: '_lambda_ar_store', expire_after: 120.minutes
