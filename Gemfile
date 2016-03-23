6e2602f905a62e3cf0504f905afa3d4b13266270
Michael Ball <cycomachead@gmail.com>
2016-03-23 00:56:57 -0700

    Use Redis as a session store

--- Gemfile
+++ Gemfile
@@ -46,8 +46,9 @@ gem 'pg'
 
 # LTI
 gem 'dce_lti', path: './dce-lti/'
-# The master branch fixes deprecation warnings on rails 2.3
-gem 'activerecord-session_store', :git => 'https://github.com/rails/activerecord-session_store.git'
+# Not using postgres due to Heroku's row limit.
+gem 'redis-session-store'
+
 
 # SPECIFIC APIS BECAUSE EDUC APIS ARE HELL
 gem 'canvas-api'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_lambda_session'
+Rails.application.config.session_store :redis_session_store, {
+  key: '_lambda_session',
+  redis: {
+    db: 2,
+    expire_after: 120.minutes,
+    key_prefix: 'lambda:session:',
+    host: 'localhost', # Redis host name, default is localhost
+    port: 6379   # Redis port, default is 6379
+  }
+}
 
-# LTI Keys
-# Rails.application.config.session_store :active_record_store, key: '_lambda_ar_store', expire_after: 120.minutes
