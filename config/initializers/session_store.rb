ef3ef6d680f898e66909a73c5b43c743074acc31
Michael Ball <cycomachead@gmail.com>
2016-03-24 18:20:22 -0800

    Use a redis store on heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,13 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
+if ENV['REDIS_URL'] != nil
+  redis_url = ENV['REDIS_URL']
+else
+  redis_url = 'redis://localhost:6379'
+end
+
 Rails.application.config.session_store :redis_session_store, {
   key: '_lambda_session',
   redis: {
     db: 2,
     expire_after: 120.minutes,
     key_prefix: 'lambda:session:',
-    host: 'localhost', # Redis host name, default is localhost
-    port: 6379   # Redis port, default is 6379
+    url: redis_url
   }
 }
 
