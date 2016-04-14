697884ca773c99282a1eaaaff0091cc4f653692d
Michael Ball <cycomachead@gmail.com>
2016-04-14 14:36:50 -0700

    Fix logging in to work and add flashes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,12 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    render :text => auth_hash.inspect
-
     begin
       @user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = @user.id
       flash[:success] = "Welcome, #{@user.name}!"
-    rescue
+    rescue Exception => e
+      puts e.message
       flash[:warning] = "There was an error while trying to authenticate you..."
     end
     redirect_to root_path
