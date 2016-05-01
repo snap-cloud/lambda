e551a68c09bf371a1959106cb0d4e641d729e931
Michael Ball <cycomachead@gmail.com>
2016-05-01 02:45:35 -0700

    Cant get a user name on logout..oops

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
   def destroy
     if current_user
       session.delete(:user_id)
-      flash[:success] = "See you, #{@user.name}!"
+      flash[:success] = "See you!"
     end
     redirect_to root_path
   end
