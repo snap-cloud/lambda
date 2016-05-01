f3e333f6807fe1911c0844ba13c0b1fbec5c1dbb
Michael Ball <cycomachead@gmail.com>
2016-05-01 02:40:25 -0700

    Fix some stupid bugs

--- dce-lti/app/concerns/dce_lti/session_helpers.rb
+++ dce-lti/app/concerns/dce_lti/session_helpers.rb
@@ -1,5 +1,6 @@
 module DceLti
   module SessionHelpers
+    def valid_lti_request?(request)
       tool_provider.valid_request?(request) &&
         Nonce.valid?(tool_provider.oauth_nonce) &&
         TimestampValidator.valid?(tool_provider.oauth_timestamp)
