6e81a4749ed5cdb66c1e78e44ac883390932bb90
Michael Ball <cycomachead@gmail.com>
2016-05-01 01:08:51 -0700

    Fix google login to include email

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
-           scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
+    {
+      name: "google",
+      access_type: 'offline',
+      scope: "email, profile, plus.me",
+      prompt: "select_account",
+      image_aspect_ratio: "square",
+      image_size: 50
+    }
 
  OmniAuth.config.on_failure = Proc.new do |env|
    SessionsController.action(:auth_failure).call(env)
