59051d440a6a9929e77322cf8d809c6937e8a403
Michael Ball <cycomachead@gmail.com>
2017-07-01 22:17:50 -0700

    Update submissions with new user Ids

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702051010) do
+ActiveRecord::Schema.define(version: 20170702051347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
