217bc9ee91a259652da6884d6be19d5739484d98
Michael Ball <cycomachead@gmail.com>
2017-08-23 22:51:03 -0700

    de-indent file (no changes)

--- app/models/nonce.rb
+++ app/models/nonce.rb
@@ -12,20 +12,18 @@
 #  index_nonces_on_nonce  (nonce) UNIQUE
 #
 
-module DceLti
-  class Nonce < ActiveRecord::Base
-    def self.clean
-      delete_all(['created_at < ?', Time.now - 6.hours])
-    end
+class Nonce < ActiveRecord::Base
+  def self.clean
+    delete_all(['created_at < ?', Time.now - 6.hours])
+  end
 
-    def self.valid?(nonce)
-      begin
-        self.create!(nonce: nonce)
-        true
-      rescue => e
-        Rails.logger.warn(%Q|Creating nonce failed: "#{nonce}"|)
-        false
-      end
+  def self.valid?(nonce)
+    begin
+      self.create!(nonce: nonce)
+      true
+    rescue => e
+      Rails.logger.warn(%Q|Creating nonce failed: "#{nonce}"|)
+      false
     end
   end
 end
